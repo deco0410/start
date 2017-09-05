b9b6c88c980f9bbd18e1ad6f07f2892194ed3a16
deco <9111616@qq.com>
2017-09-05 16:47:42 +0800

    0905 at company

---
+++ application/build.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'test' => [
+        '__dir__' => ['controller', 'model', 'view'],
+        'controller' => ['User', 'UserType'],
+        'model' => ['User', 'UserType'],
+        'view' => ['index/index', 'index/test'],
+    ],
+];
