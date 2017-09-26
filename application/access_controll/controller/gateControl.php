47a5d27d0d4e0df241d291e04aff46bc0ced4641
deco <9111616@qq.com>
2017-09-26 09:06:23 +0800

    0925 at company

---
+++ application/access_controll/controller/gateControl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/25 0025
+ * Time: 上午 11:55
+ */
--- application/index/controller/Blog.php
+++ application/index/controller/Blog.php
@@ -2,12 +2,33 @@
 namespace app\index\controller;
 
 use think\Controller;
+use app\index\model\User as UserModel;
+use think\Db;
 
 class Blog extends Controller
 {
-    public function index($author){
-        $this->assign('author', $author);
-        $this->fetch('blog/index');
+    public function index(){
+        $author = input('author');
+        $check_author = UserModel::get(['nickname'=>$author]);
+
+        if($check_author){
+            $this->assign('author', $author);
+            return $this->fetch('blog/index');
+
+        }else{
+            return $this->fetch('index/index');
+        }
     }
 
+
+
+
+
+
+
+
+
+
+
 }
+
